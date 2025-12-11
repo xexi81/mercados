f9e158530aa0fa66f0e79711f72d0c38966432a9
xexi81 <sergionunez90@gmail.com>
2025-12-11 09:33:39 +0100

    Pantalla de login

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../data/frases_iniciales.dart';
 import 'login_page.dart';
+import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -22,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => const LoginPage()),
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
       );
     });
   }
